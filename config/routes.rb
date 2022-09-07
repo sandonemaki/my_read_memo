6732c26641fd6210b5382c05f0ead8d74e5c487c
sandonemaki <kamuramaki@gmail.com>
2022-09-07 16:46:16 +0900

    rootとget /book&pages/、post /book&pages/を登録

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "book&pages" => "book&pages#new"
+  post "book&pages" => "book&pages#create"
+
+  root to: "books#index"
 end
