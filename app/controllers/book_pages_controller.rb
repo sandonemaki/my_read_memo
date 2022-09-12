f612233f6305185f2745d117f47d5aa67e479317
sandonemaki <kamuramaki@gmail.com>
2022-09-13 02:04:31 +0900

    本の情報を登録して表示できるようにした

--- app/controllers/book_pages_controller.rb
+++ app/controllers/book_pages_controller.rb
@@ -1,28 +1,40 @@
 class BookPagesController < ApplicationController
-  require_relative '../module/state'
+  require_relative '../modules/state'
 
   def new
     book = Book.new
+    book_title = book.title
+    book_author_1 = book.author_1
+    book_author_2 = book.author_2
+    book_publisher = book.publisher
+    book_total_page = book.total_page
+    create_book_view_model =
+      BookViewModel::NewViewModel.new(
+        title: book_title,
+        author_1: book_author_1,
+        author_2: book_author_2,
+        publisher: book_publisher,
+        total_page: book_total_page,
+      )
+    render("new", locals:{book: create_book_view_model})
   end
 
   def create
-    post = Book.new(
+    book = Book.new(
       title: params[:title],
       author_1: params[:author_1],
       author_2: params[:author_2],
       publisher: params[:publisher],
       total_page: params[:total_page],
-      reading_state: State::READING_STATE[:randoku]
     )
     if book.save
-      redirect_to("/book_pages")
-    else
+      # redirect_to("/book_pages")
+    # else
       book_title = book.title
       book_author_1 = book.author_1
       book_author_2 = book.author_2
       book_publisher = book.publisher
       book_total_page = book.total_page
-      book_reading_state = book.reading_state
       create_book_view_model =
         BookViewModel::NewViewModel.new(
           title: book_title,
@@ -30,7 +42,6 @@ class BookPagesController < ApplicationController
           author_2: book_author_2,
           publisher: book_publisher,
           total_page: book_total_page,
-          reading_satate: book_reading_state
         )
       render("new", locals:{book: create_book_view_model})
     end
--- app/view_models/book_view_model.rb
+++ app/view_models/book_view_model.rb
@@ -1,15 +1,14 @@
 class BookViewModel
 
   class NewViewModel
-    attr_reader :title, :author_1, :author_2, :publisher, :total_page, :reading_state
+    attr_reader :title, :author_1, :author_2, :publisher, :total_page
 
-    def initialize(title:, author_1:, author_2:, publisher:, total_page:, reading_state:)
+    def initialize(title:, author_1:, author_2:, publisher:, total_page:)
       @title = title
       @author_1 = author_1
       @author_2 = author_2
       @publisher = publisher
       @total_page = total_page
-      @reading_state = reading_state
     end
   end
 end
