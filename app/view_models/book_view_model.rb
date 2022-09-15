1087936bee302fba402280316710259e85121ab6
sandonemaki <kamuramaki@gmail.com>
2022-09-15 21:36:48 +0900

    show_view_modelを定義した

--- app/view_models/book_view_model.rb
+++ app/view_models/book_view_model.rb
@@ -12,4 +12,17 @@ class BookViewModel
       @errors = errors
     end
   end
+
+  class ShowViewModel
+    attr_reader :id, :title, :author_1, :author_2, :publisher, :total_page
+
+    def initialize(id:, title:, author_1:, author_2:, publisher:, total_page:)
+      @id = id
+      @title = title
+      @author_1 = author_1
+      @author_2 = author_2
+      @publisher = publisher
+      @total_page = total_page
+    end
+  end
 end
