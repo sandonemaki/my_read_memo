a240d64f78bc798ff5e996eb5da27f04be7b2873
sandonemaki <kamuramaki@gmail.com>
2022-09-15 20:22:49 +0900

    登録した本の情報が確認できて画像を投稿できるページのroutesを設定した

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "book_pages" => "book_pages#new"
-  post "book_pages" => "book_pages#create"
+  get "book_pages" => "book_pages#new"      # new_book_page_path
+  post "book_pages" => "book_pages#create"  # book_pages_path
+  get "book_pages/:id" => "book_pages#show" # book_page_path
+  post "book_pages/:book_id/imgs" => "imgs#create" # book_page_imgs_path
 
   root to: "books#index"
 end
