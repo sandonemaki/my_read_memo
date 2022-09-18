172077face685f0211e4e01838e1b8b673806dfb
sandonemaki <kamuramaki@gmail.com>
2022-09-18 17:28:58 +0900

    public下にbook.idディレクトリを作成しimg画像を保存。拡張子が.HEIC/.heicの場合.jpgに置き換え。

--- app/controllers/book_pages/imgs_controller.rb
+++ app/controllers/book_pages/imgs_controller.rb
@@ -5,7 +5,18 @@ class BookPages::ImgsController < ApplicationController
   def create
     if params[:page_imgs]
       page_imgs = params[:page_imgs]
-      book = Book.find_by(id: params[:id])
-  end
+      book = Book.find_by(id: params[:book_id])
+      page_image_names = []
 
+      # public下にbook_idで全てのディレクトリを作成
+      FileUtils.mkdir_p("public/#{book.id}/")
+      page_imgs.each do |page_img|
+        page_img_extname = File.extname(page_img.original_filename)
+        if page_img_extname.match("\.HEIC$|\.heic$")
+          jpg_imgname =
+            "public/#{book.id}/#{page_img.original_filename.sub(/.HEIC$|.heic$/, ".jpg")}"
+        end
+      end
+    end
+  end
 end
