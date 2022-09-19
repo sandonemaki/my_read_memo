8f3d8097be07648bf0c7f7b9e7ec3a54f8545d9a
sandonemaki <kamuramaki@gmail.com>
2022-09-19 10:35:24 +0900

    一時的なtempディレクトリを作成して拡張子が.HEIC/.heicの画像を格納しjpgフォーマットに変換した

--- app/controllers/book_pages/imgs_controller.rb
+++ app/controllers/book_pages/imgs_controller.rb
@@ -8,14 +8,23 @@ class BookPages::ImgsController < ApplicationController
       book = Book.find_by(id: params[:book_id])
       page_img_names = []
 
-      # public下にbook_idで全てのディレクトリを作成
       FileUtils.mkdir_p("public/#{book.id}/")
-      page_imgs.each do |page_img|
+      # ファイル名の保存
+      page_imgs.each_with_index do |page_img|
         page_img_extname = File.extname(page_img.original_filename)
         if page_img_extname.match("\.HEIC$|\.heic$")
           jpg_imgname =
             "public/#{book.id}/#{page_img.original_filename.sub(/.HEIC$|.heic$/, ".jpg")}"
           page_img_names << jpg_imgname
+
+          # 実体の保存
+          Dir.mktmpdir do |tmpdir|
+            File.binwrite("#{tmpdir}/#{page_img.original_filename}", page_img.read)
+            system('magick modrify -format jpg '+tmpdir+'/*.HEIC')
+            FileUtils.mv(Dir.glob("#{tmpdir}/*jpg"), "public/#{book.id}/")
+          end
+
+
         end
       end
     end
