b92d71c598446068f53f88bf90cc1d6a8442f3e2
sandonemaki <kamuramaki@gmail.com>
2022-10-06 07:14:08 +0900

    本のタイトル、著者の入力許可文字数を修正した

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,9 +1,9 @@
 class Book < ApplicationRecord
   has_many :randoku_imgs, dependent: :destroy
   validates :title, presence: {message: "タイトルを入力してください"}
-  validates :title, length: {maximum: 10, message: "10文字以内で入力してください"}
+  validates :title, length: {maximum: 30, message: "30文字以内で入力してください"}
   validates :author_1, presence: {message: "著者を入力してください"}
-  validates :author_1, length: {maximum: 10, message: "10文字以内で入力してください"}
+  validates :author_1, length: {maximum: 30, message: "30文字以内で入力してください"}
 
   def show_for_book_view_model(book)
     book_id = book.id
