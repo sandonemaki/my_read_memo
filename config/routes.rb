372fdc4eaeefdbd250c89ebe71db639ef7ac16a8
sandonemaki <kamuramaki@gmail.com>
2022-10-17 23:55:48 +0900

    /createの保存が失敗した際にリダイレクトした場合newにリクエストが飛ぶようにした

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "book_pages/new" => "book_pages#new"  # new_book_page_path
-  post "book_pages" => "book_pages#create"  # book_pages_path
+  post "book_pages/create" => "book_pages#create"  # book_pages_path
+  get "book_pages/create" => "book_pages#new"  # book_pages_path
   get "book_pages/:id" => "book_pages#show" # book_page_path
   post "book_pages/:book_id/imgs" => "book_pages/imgs#create" # book_page_imgs_path
   root to: "book_pages#index"
