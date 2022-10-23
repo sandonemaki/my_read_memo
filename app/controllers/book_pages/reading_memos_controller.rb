53e18c63812f19b2203880f512191554fc4e9241
sandonemaki <kamuramaki@gmail.com>
2022-10-24 01:02:41 +0900

    viw_modelにbookインスタンスを渡した

--- app/controllers/book_pages/reading_memos_controller.rb
+++ app/controllers/book_pages/reading_memos_controller.rb
@@ -1,2 +1,7 @@
 class BookPages::ReadingMemosController < ApplicationController
+  def index
+    book = Book.find_by(id: params[:id])
+    book_view_model = ViewModel::ReadingMemoViewModel.new(book: book)
+    render("index", locals: {book: book_view_model})
+  end
 end
