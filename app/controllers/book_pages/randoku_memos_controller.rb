53a087aaedbde6b013a4258195c054d4a6ba7ae8
sandonemaki <kamuramaki@gmail.com>
2022-10-26 05:44:13 +0900

    乱読メモのindexにメモと本と乱読画像の必要な情報が表示されるようにした

--- app/controllers/book_pages/randoku_memos_controller.rb
+++ app/controllers/book_pages/randoku_memos_controller.rb
@@ -1,2 +1,18 @@
 class BookPages::RandokuMemosController < ApplicationController
+  def index
+    puts book = Book.find_by(id: params[:book_id])
+    randoku_img_first_post_filename =
+      if book.randoku_imgs.exists?(id: 1)
+        book.randoku_imgs.find_by(id: 1).name
+      else
+        ""
+      end
+    randoku_memos_all = book.randoku_memos.all
+    book_view_model = ViewModel::RandokuMemos.new(
+      book: book,
+      randoku_img_first_post_filename: randoku_img_first_post_filename
+    )
+    render("index", locals:{book: book_view_model})
+  end
+
 end
---
+++ app/view_models/view_model/reading_memos_view_model.rb
@@ -0,0 +1,35 @@
+module ViewModel
+
+  class RandokuMemos
+    attr_reader :id, :title, :author_1, :reading_state, :publisher,
+      :randoku_img_read_again_count, :randoku_img_finish_read_count,
+      :randoku_img_first_post_filename,
+      :randoku_memos_all, :randoku_memos_all_count
+
+
+    def initialize(book:, randoku_img_first_post_filename:)
+      @id = book.id
+      @title = book.title
+      @author_1 = book.author_1
+      @reading_state =
+        case book.reading_state
+        when State::READING_STATE.key("乱読")
+          "乱読"
+        when State::READING_STATE.key("精読")
+          "精読"
+        else
+          "通読"
+        end
+      @publisher = book.publisher
+
+      reading_state = book.randoku_imgs.group('reading_state').size
+      @randoku_img_read_again_count  = reading_state[0] ||= 0 # 未読の数(0)
+      @randoku_img_finish_read_count = reading_state[1] ||= 0 # 既読の数(0)
+      @randoku_img_first_post_filename = randoku_img_first_post_filename
+
+      @randoku_memos_all = book.randoku_memos.all
+      @randoku_memos_all_count = @randoku_memos_all.size
+
+    end
+  end
+end
