3dc94ea4264e0bfb167583b9173d96ca08343bc4
sandonemaki <kamuramaki@gmail.com>
2022-10-25 15:30:27 +0900

    設計を変更して1modelにつき1controllerに変更した

--- app/controllers/book_pages/reading_memos_controller.rb
+++
@@ -1,7 +0,0 @@
-class BookPages::ReadingMemosController < ApplicationController
-  def index
-    book = Book.find_by(id: params[:id])
-    book_view_model = ViewModel::ReadingMemoViewModel.new(book: book)
-    render("index", locals: {book: book_view_model})
-  end
-end
--- app/helpers/book_pages/reading_memos_helper.rb
+++
@@ -1,2 +0,0 @@
-module BookPages::ReadingMemosHelper
-end
