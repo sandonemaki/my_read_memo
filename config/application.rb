8552c41f27de7f4414dcf2169413a66e02d90c5b
sandonemaki <kamuramaki@gmail.com>
2022-10-31 16:17:42 +0900

    i18n、日本時間の設定をした

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,8 @@ module MyReadMemo
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = 'Tokyo'
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
