af1b5dbf12e10255f4ed6da88aafcd396f5f3481
sandonemaki <kamuramaki@gmail.com>
2022-11-01 22:26:34 +0900

    if分岐を三項演算子にした

--- app/controllers/book_pages/randoku_memos_controller.rb
+++ app/controllers/book_pages/randoku_memos_controller.rb
@@ -2,11 +2,7 @@ class BookPages::RandokuMemosController < ApplicationController
   def index
     book = Book.find_by(id: params[:book_id])
     randoku_img_first_post_filename =
-      if book.randoku_imgs.exists?(id: 1)
-        puts book.randoku_imgs.find_by(id: 1).name
-      else
-        ""
-      end
+      book.randoku_imgs.exists?(id: 1) ? book.randoku_imgs.find_by(id: 1).name : ""
     book_view_model = ViewModel::RandokuMemos.new(
       book: book,
       randoku_img_first_post_filename: randoku_img_first_post_filename
