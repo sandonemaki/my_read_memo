ebf3399a59054875e4aa0279810265bf26b25215
sandonemaki <kamuramaki@gmail.com>
2022-11-01 21:56:34 +0900

    構文エラーが出たため対応した

--- app/controllers/book_pages/seidoku_memos_controller.rb
+++ app/controllers/book_pages/seidoku_memos_controller.rb
@@ -1,16 +1,13 @@
 class BookPages::SeidokuMemosController < ApplicationController
   def new
     book = Book.find_by(id: params[:book_id])
-    book_view_model = ViewModel::SeidokuMemos.new(
+    book_view_model = ViewModel::SeidokuMemoNew.new(
       book: book,
     )
     render("new", locals:{book: book_view_model})
   end
 
   def create
-    puts "--------"
-    puts content_state: params[:value].class
-    puts "--------"
     book = Book.find_by(id: params[:book_id])
     seidoku_memos = book.seidoku_memos.new(
       content_state: params[:value].to_i,
