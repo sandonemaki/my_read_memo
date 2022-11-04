2f6fcdf4b3bc8ca5425e4d9b2b09c5b6c7dda48c
sandonemaki <kamuramaki@gmail.com>
2022-11-04 20:04:32 +0900

    乱読・精読の前回の学習履歴テーブルをmigrateした

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_31_092935) do
+ActiveRecord::Schema.define(version: 2022_11_04_095820) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 2022_10_31_092935) do
     t.integer "reading_state", default: 0, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "randoku_histories", force: :cascade do |t|
+    t.string "path", default: ""
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "book_id", default: 0
   end
 
   create_table "randoku_imgs", force: :cascade do |t|
@@ -42,6 +49,13 @@ ActiveRecord::Schema.define(version: 2022_10_31_092935) do
   create_table "randoku_memos", force: :cascade do |t|
     t.text "content"
     t.integer "book_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "seidoku_histories", force: :cascade do |t|
+    t.string "path", default: ""
+    t.integer "book_id", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
