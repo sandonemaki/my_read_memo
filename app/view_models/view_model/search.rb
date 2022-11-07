9ee76bbed100b89ea2ede2bafa3fe933d05d9b87
sandonemaki <kamuramaki@gmail.com>
2022-11-08 01:13:43 +0900

    viewでmemoの本のタイトルや著者が表示できるようにした

--- app/view_models/view_model/search.rb
+++ app/view_models/view_model/search.rb
@@ -4,12 +4,22 @@ module ViewModel
 
     def initialize(all_randoku_state_books:, all_seidoku_state_books:, content_type:,
                    randoku_memo_type:, seidoku_memo_type:)
-      @selected_memos =
+      selected_memos =
         case content_value
         when content_value.include?("randoku")
           all_randoku_state_books.where(content_state: "#{content_type.match(/[(0-2)]/).to_s}")
         when content_value.include?("seidoku")
           all_seidoku_state_books.where(content_state: "#{content_type.match(/[(0-3)]/).to_s}")
+        end
+      @selected_memos =
+        selected_memos.map do |memo|
+          { memo_content: memo.content,
+            memo_created_at: I18n.l(memo.created_at, format: :short),
+            memo_content_state: book.memo.content_state,
+            title: Book.find_by(id: memo.book_id).title,
+            author: Book.find_by(id: memo.book_id).author_1,
+            reading_state: Book.find_by(id: memo.book_id).reading_state
+          }
         end
       @randoku_memo_type = randoku_memo_type
       @seidoku_memo_type = seidoku_memo_type
