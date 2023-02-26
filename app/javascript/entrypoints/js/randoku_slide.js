1062a5204d17ddfa4bff05a965ad198b5cd8f080
sandonemaki <kamuramaki@gmail.com>
2023-02-26 19:03:57 +0900

    htmlからモーダルのエレメントを取得した。

--- app/javascript/entrypoints/js/randoku_slide.js
+++ app/javascript/entrypoints/js/randoku_slide.js
@@ -3,10 +3,19 @@ import 'swiper/swiper-bundle.css';
 import '../styles/slide.css';
 
 document.addEventListener('DOMContentLoaded', function() {
-  const img = document.querySelector(".img");
-  const updated_at = document.querySelector(".updated_at");
+  const img = document.querySelector("#img");
+  const updated_at = document.querySelector("#updated_at");
   const img_name = document.querySelector(".img_name");
+	// モーダル
+	const modal = document.querySelector("#modal")
+	const modalImage = document.querySelector("#modal-image")
+	const closeButton = document.querySelector("#close-button")
+	// swiper
+	const prevButton = document.querySelector("#button-prev")
+	const nextButton = document.querySelector("#button-next")
 
+
+	// 画像の高さを揃える
   function matchHeight(elements) {
     const targets =
       Array.from(document.querySelectorAll(elements));
