d2ec0c94e8d9dec43cf11dd20a206079923a92b6
sandonemaki <kamuramaki@gmail.com>
2023-02-26 21:21:06 +0900

    モーダルのcssを追加、モーダル操作のエレメントを取得

--- app/javascript/entrypoints/js/randoku_slide.js
+++ app/javascript/entrypoints/js/randoku_slide.js
@@ -5,17 +5,41 @@ import '../styles/slide.css';
 document.addEventListener('DOMContentLoaded', function() {
   const img = document.querySelector("#img");
   const updated_at = document.querySelector("#updated_at");
-  const img_name = document.querySelector(".img_name");
-	// モーダル
-	const modal = document.querySelector("#modal")
-	const modalImage = document.querySelector("#modal-image")
-	const closeButton = document.querySelector("#close-button")
-	// swiper
-	const prevButton = document.querySelector("#button-prev")
-	const nextButton = document.querySelector("#button-next")
+  const img_name = document.querySelector("#img_name");
+  // Todo:htmlの画像のリンクにidを書いて取得する
+  const links = document.querySelector("a[id^='photo-link-']");
+  // モーダル
+  const modal = document.querySelector("#modal")
+  const modalImage = document.querySelector("#modal-image")
+  const closeButton = document.querySelector("#close-button")
+  // swiper
+  const prevButton = document.querySelector("#button-prev")
+  const nextButton = document.querySelector("#button-next")
 
 
-	// 画像の高さを揃える
+
+  // モーダルを閉じる
+  closeButton.addEventListener("click", (e) => {
+    modal.style.display = "none";
+  });
+  window.addEventListener("click", (e) => {
+    if(e.target === modal) {
+      modal.style.display = "none";
+    }
+  });
+
+  // モーダルを開く
+  for (let i = 0; i < links.length; i++) {
+    links[i].addEventListner("click", (e) => {
+      const photoId = e.target.id.split("-")[2];
+      // Todo:クリックされた画像を拡大表示する
+      modal.style.display = "block";
+    });
+  }
+
+
+
+  // 画像の高さを揃える
   function matchHeight(elements) {
     const targets =
       Array.from(document.querySelectorAll(elements));
