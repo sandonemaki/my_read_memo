442ee3aab72c07924bf478a89b956fe0722d3697
sandonemaki <kamuramaki@gmail.com>
2023-03-11 07:37:10 +0900

    js0745

--- app/javascript/entrypoints/js/randoku_slide.js
+++ app/javascript/entrypoints/js/randoku_slide.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function() {
   const imgName = document.querySelector("#sw_img_name");
   const imgs = document.querySelectorAll("#swiper_img");
   const updatedAt = document.querySelector("#sw_updated_at");
-  const readBtn = document.querySelector("#sw-read-btn");
+  const readBtn = document.querySelector(".sw_read_btn");
   // モーダル
   const modal = document.querySelector(".sw-modal")
   const closeButton = document.querySelector("#sw-close-btn")
@@ -47,39 +47,44 @@ document.addEventListener('DOMContentLoaded', function() {
     modal.classList.remove('active');
   });
 
+  console.log(readBtn)
   // 未読・既読/toggle-button
   readBtn.addEventListener('click', async () => {
-    const readingId = parseInt(bookBtn.getAttribute('data-reading-id'));
+    if (readBtn) {
+      console.log("gogog")
+      const readingId = parseInt(readBtn.getAttribute('data-reading-id'));
 
-    try {
-      const response = await fetch('/books/show', {
-        method: 'POST',
-        credentials: 'same-origin',
-        headers: {
-          'Content-Type': 'application/json',
-          'X-CSRF-Token': getCsrfToken()
-        },
-        body: JSON.stringify({ readingId: readingId }),
-      });
+      try {
+        const response = await fetch('/show', {
+          method: 'POST',
+          credentials: 'same-origin',
+          headers: {
+            'Content-Type': 'application/json',
+            'X-CSRF-Token': getCsrfToken()
+          },
+          body: JSON.stringify({ readingId: readingId }),
+        })
 
-      if (!response.ok) {
-        throw new Error(response.statusText);
+        if (!response.ok) {
+          throw new Error(response.statusText);
+        }
+
+        console.log("iiiii")
+        // 成功時
+        readBtn.classList.toggle('completion');
+        if (readBtn.classList.contains('completion')) {
+          readBtn.setAttribute('data-reading-id', '1');
+          readBtn.textContent = '完了済み';
+        } else {
+          readBtn.setAttribute('data-reading-id', '0');
+          readBtn.textContent = '読んだ!';
+        }
+
+      } catch (error) {
+        console.error('エラーが発生しました', error);
       }
-      // 成功時
-      readBtn.classList.toggle('completion');
-      if (readBtn.classList.contains('completion')) {
-        readBtn.setAttribute('data-reading-id', '1');
-        readBtn.textContent = '完了済み';
-      } else {
-        readBtn.setAttribute('data-reading-id', '0');
-        readBtn.textContent = '読んだ!';
-      }
-
-    } catch (error) {
-      console.error('エラーが発生しました', error);
     }
   });
-
   const getCsrfToken = () => {
     const metas = document.getElementsByTagName('meta');
     for (let meta of metas) {
