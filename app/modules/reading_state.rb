8582851406615adff6a038295794a0b3b9d9826b
sandonemaki <kamuramaki@gmail.com>
2023-03-15 20:20:37 +0900

    変更予定のrandoku_imgsテーブルのカラムreading_state に対するコメントを追加

--- app/modules/reading_state.rb
+++ app/modules/reading_state.rb
@@ -4,6 +4,7 @@ module State
 
     def initialize(book:)
       @book_total_pages = book.total_page
+      # Todo: randoku_imgsのカラム「reading_state」はis_already_readに変更予定
       @randoku_imgs_count = book.randoku_imgs.group('reading_state').size
     end
   end
