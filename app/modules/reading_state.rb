f1f24e5eff328bf2d2f0df835fd3dd4b01cf54b3
sandonemaki <kamuramaki@gmail.com>
2023-03-15 20:23:05 +0900

    一冊の本の状態が乱読/精読/通読かを判定するメソッドを追加

--- app/modules/reading_state.rb
+++ app/modules/reading_state.rb
@@ -7,5 +7,11 @@ module State
       # Todo: randoku_imgsのカラム「reading_state」はis_already_readに変更予定
       @randoku_imgs_count = book.randoku_imgs.group('reading_state').size
     end
+
+    def calculated_reading_status
+      return State::READING_STATE.key("乱読") if randoku_status?
+      return State::READING_STATE.key("精読") if seidoku_status?
+      State::READING_STATE.key("通読")
+    end
   end
 end
