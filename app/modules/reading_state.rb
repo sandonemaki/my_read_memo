a238d5afece3c3c5daa8cf3176e57554646472b5
sandonemaki <kamuramaki@gmail.com>
2023-03-15 20:18:08 +0900

    演算から本の状態（乱読/精読/通読）を返すReadingStatusクラスを作成

--- app/modules/reading_state.rb
+++ app/modules/reading_state.rb
@@ -1,2 +1,10 @@
 module State
+  class ReadingStatus
+    attr_reader :randoku_imgs_count, :book_total_pages
+
+    def initialize(book:)
+      @book_total_pages = book.total_page
+      @randoku_imgs_count = book.randoku_imgs.group('reading_state').size
+    end
+  end
 end
