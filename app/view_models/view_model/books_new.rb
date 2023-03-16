a4a707a206c00700d28f1dcdac92f3b7d78dc0c8
sandonemaki <kamuramaki@gmail.com>
2023-03-17 01:15:15 +0900

    books/new 周りの変数名を適切なものに変更

--- app/view_models/view_model/books_new.rb
+++ app/view_models/view_model/books_new.rb
@@ -1,13 +1,13 @@
 module ViewModel
 
   class BooksNew
-    attr_reader :id, :title, :author_1, :total_page, :publisher, :errors
+    attr_reader :id, :title, :author, :total_page, :publisher, :errors
 
     # @param book [Book] 本モデル
     def initialize(book:)
       @id = book.id
       @title = book.title
-      @author_1 = book.author_1
+      @author = book.author_1
       @total_page = book.total_page
       @publisher = book.publisher
       @errors = book.errors
