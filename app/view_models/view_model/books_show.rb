85b32d0f43093e9398674dda3bfe02161773067d
sandonemaki <kamuramaki@gmail.com>
2023-03-17 00:23:33 +0900

    book/:idに関する変数名を内容に即したものに修正

--- app/view_models/view_model/books_show.rb
+++ app/view_models/view_model/books_show.rb
@@ -1,18 +1,18 @@
 module ViewModel
 
   class BooksShow
-    attr_reader :id, :title, :author_1, :total_page, :reading_state,
-      :publisher, :errors, :randoku_img_read_again_count, :randoku_img_finish_read_count,
-      :randoku_img_file_names, :randoku_imgs_all_count, :randoku_imgs_all
+    attr_reader :id, :title, :author, :total_page, :reading_progress,
+      :publisher, :errors, :randoku_imgs_unread_count, :randoku_imgs_alreadyread_count,
+      :randoku_imgs_file_names, :randoku_imgs_all_count, :randoku_imgs_all
 
 
     # @param book [Book] 本モデル
     def initialize(book:)
       @id = book.id
       @title = book.title
-      @author_1 = book.author_1
+      @author = book.author_1
       @total_page = book.total_page
-      @reading_state =
+      @reading_progress =
         case book.reading_state
         when State::READING_STATE.key("乱読")
           "乱読"
@@ -24,10 +24,10 @@ module ViewModel
       @publisher = book.publisher
       @errors = book.errors
 
-      reading_state = book.randoku_imgs.group('reading_state').size
-      @randoku_img_read_again_count  = reading_state[0] ||= 0 # 未読の数(0)
-      @randoku_img_finish_read_count = reading_state[1] ||= 0 # 既読の数(0)
-      @randoku_img_file_names = Dir.glob("public/#{book.id}/thumb/*")
+      randoku_imgs_count = book.randoku_imgs.group('reading_state').size
+      @randoku_imgs_unread_count = randoku_imgs_count[0] ||= 0 # 未読の数(0)
+      @randoku_imgs_alreadyread_count = randoku_imgs_count[1] ||= 0 # 既読の数(0)
+      @randoku_imgs_file_names = Dir.glob("public/#{book.id}/thumb/*")
         .sort_by { |randoku_img_path| File.mtime(randoku_img_path) }
         .map { |f| f.split("/").last }
         .reverse
