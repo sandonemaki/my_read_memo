00ff6017f969cf1b6da18fae48b0722301fa0f2a
sandonemaki <kamuramaki@gmail.com>
2023-03-17 01:10:11 +0900

    不要な変数を削除

--- app/view_models/view_model/books_new.rb
+++ app/view_models/view_model/books_new.rb
@@ -1,9 +1,7 @@
 module ViewModel
 
   class BooksNew
-    attr_reader :id, :title, :author_1, :total_page, :reading_state,
-      :publisher, :errors, :randoku_img_read_again_count, :randoku_img_finish_read_count,
-      :randoku_img_file_names
+    attr_reader :id, :title, :author_1, :total_page, :publisher, :errors
 
     # @param book [Book] 本モデル
     def initialize(book:)
@@ -11,25 +9,8 @@ module ViewModel
       @title = book.title
       @author_1 = book.author_1
       @total_page = book.total_page
-      @reading_state =
-        case book.reading_state
-        when State::READING_STATE.key("乱読")
-          "乱読"
-        when State::READING_STATE.key("精読")
-          "精読"
-        else
-          "通読"
-        end
       @publisher = book.publisher
       @errors = book.errors
-
-      reading_state = book.randoku_imgs.group('reading_state').size
-      @randoku_img_read_again_count  = reading_state[0] ||= 0 # 未読の数(0)
-      @randoku_img_finish_read_count = reading_state[1] ||= 0 # 既読の数(0)
-      @randoku_img_file_names = Dir.glob("public/#{book.id}/thumb/*")
-        .sort_by { |randoku_img_path| File.mtime(randoku_img_path) }
-        .map { |f| f.split("/").last }
-        .reverse
     end
   end
 end
