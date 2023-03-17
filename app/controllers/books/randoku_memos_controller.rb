ce35eee7c1201003613b2d425393f813dd1a5caa
sandonemaki <kamuramaki@gmail.com>
2023-03-18 03:01:21 +0900

    本の状態を調べた上で学習履歴を保存

---
+++ app/controllers/books/randoku_memos_controller.rb
@@ -0,0 +1,48 @@
+class Books::RandokuMemosController < ApplicationController
+  def index
+    book = Book.find_by(id: params[:book_id])
+    new_path = "books/#{book.id}/randoku_memos/index"
+
+    # 学習履歴を保存
+    if ['0', '2'].include?(book.reading.to_s)
+      RandokuHistory.set(new_path, book.id)
+    else
+      SeidokuHistory.set(new_path, book.id)
+    end
+
+    randoku_img_first_post_filename =
+      book.randoku_imgs.exists?(id: 1) ? book.randoku_imgs.find_by(id: 1).name : ""
+    book_view_model = ViewModel::BooksRandokuMemosIndex.new(
+      book: book,
+      randoku_img_first_post_filename: randoku_img_first_post_filename
+    )
+    render("index", locals:{book: book_view_model})
+  end
+
+  def new
+    book = Book.find_by(id: params[:book_id])
+    book_view_model = ViewModel::BooksRandokuMemosNew.new(
+      book: book,
+    )
+    render("new", locals:{book: book_view_model})
+  end
+
+  def create
+    book = Book.find_by(id: params[:book_id])
+    randoku_memos = book.randoku_memos.new(
+      content_state: params[:value].to_i,
+      content: params[:content]
+    )
+    if randoku_memos.save
+      flash[:notice] = "乱読メモを保存しました"
+      redirect_to("/books/#{book.id}/randoku_memos/index")
+    else
+      book_view_model = ViewModel::BooksRandokuMemosNew.new(
+        book: book,
+        content_state: params[:value],
+        content: params[:content]
+      )
+      render("new", locals:{book: book_view_model})
+    end
+  end
+end
