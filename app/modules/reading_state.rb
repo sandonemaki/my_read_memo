56839fb11c6391469082cc4ea52dcac424866b7b
sandonemaki <kamuramaki@gmail.com>
2023-03-22 00:23:23 +0900

    本の乱読/精読/通読を判定するメソッドのアルゴリズムが間違っていたので変更

--- app/modules/reading_state.rb
+++ app/modules/reading_state.rb
@@ -1,29 +1,22 @@
 module State
-  class ReadingStatus
-    attr_reader :randoku_imgs_count, :book_total_pages
+  module ReadingState
 
-    def initialize(book:)
-      @book_total_pages = book.total_page
-      # Todo: randoku_imgsのカラム「reading_state」はis_already_readに変更予定
-      @randoku_imgs_count = book.randoku_imgs.group('reading_state').size
+    def judgement_type(totalpage:, already_read:, unread:)
+      randoku_imges = already_read + unread
+
+      if randoku_images < (totalpage*(1.0/8.0))
+        State::ReadingState::Randoku.new
+      elsif randoku_images >= (totalpage*(1.0/8.0)) && unread < (totalpage*(1.0/4.0))
+        State::ReadingState::Seidoku.new
+      elsif randoku_images >= (totalpage*(1.0/8.0)) && unread >= (totalpage*(1.0/4.0))
+        State::ReadingState::Tudoku.new
+      end
     end
 
-    def calculated_reading_status
-      return State::READING_STATE.key("乱読") if randoku_status?
-      return State::READING_STATE.key("精読") if seidoku_status?
-      State::READING_STATE.key("通読")
-    end
 
-    def randoku_status?
-      randoku_imgs_count < book_total_pages / 8
-    end
-
-    def seidoku_status?
-      unread_randoku_imgs_count >= book_total_pages / 8 && unread_randoku_imgs_count < book_total_pages / 4
-    end
-
-    def unread_randoku_imgs_count
-      randoku_imgs_count[0] ||= 0 # 未読の数(0)
-    end
+    class Randoku; end
+    class Seidoku; end
+    class Tudoku; end
   end
+
 end
