0e4a88fcd1cffc12a352840a826a6a313abc469a
sandonemaki <kamuramaki@gmail.com>
2023-03-23 01:17:46 +0900

    RandokuHistoryのレコードをcreated_atの検索に変更

--- app/models/randoku_history.rb
+++ app/models/randoku_history.rb
@@ -1,7 +1,7 @@
 class RandokuHistory < ApplicationRecord
 
   def self.set(new_path, book_id)
-    randoku_history = RandokuHistory.find_or_initialize_by(id: 1)
+    randoku_history = RandokuHistory.order(:created_at).first_or_initialize
     randoku_history.path = new_path
     randoku_history.book_id = book_id
     randoku_history.save
