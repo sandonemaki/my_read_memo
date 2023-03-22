ddae4002edb5f21b79ec3fedcb01f5d7938d9b8f
sandonemaki <kamuramaki@gmail.com>
2023-03-23 01:44:34 +0900

    ViewModel::BooksRandokuMemosNewの初期値に設定する変数名を変更

--- app/view_models/view_model/books_randoku_memos_new.rb
+++ app/view_models/view_model/books_randoku_memos_new.rb
@@ -1,17 +1,18 @@
 module ViewModel
   class BooksRandokuMemosNew
     attr_reader :id, :title, :randoku_memo_time_now, :randoku_memo_q, :randoku_memo_bkg,
-      :randoku_memo_content_state, :randoku_memo_content
+      :randoku_memo_content_type, :randoku_memo_content
 
-    def initialize(book:, content_state: "", content: "")
+    def initialize(book:, randokumemo_content_type: nil, randokumemo_content: "")
       @id = book.id
       @title = book.title
 
       @randoku_memo_time_now = I18n.l(Time.now, format: :short)
       @randoku_memo_q = State::RANDOKU_MEMO_Q
       @randoku_memo_bkg = State::RANDOKU_MEMO_BKG
-      @randoku_memo_content_state = content_state
-      @randoku_memo_content = content
+      # セレクトボックのparams[:value].first
+      @randoku_memo_content_type = randokumemo_content_type
+      @randoku_memo_content = randokumemo_content
     end
   end
 end
