be68c638d7c89d46736a37ca910aef5d865a30b3
sandonemaki <kamuramaki@gmail.com>
2023-03-22 20:35:53 +0900

    ボッチ演算子を削除、memoが一枚もない場合の処理を追加。ボッチ演算子を削除

--- app/view_models/view_model/home_search.rb
+++ app/view_models/view_model/home_search.rb
@@ -4,32 +4,37 @@ module ViewModel
 
     def initialize(all_randoku_state_books:, all_seidoku_state_books:, content_type:,
                    randoku_memo_type:, seidoku_memo_type:)
-      @content_type = content_type
+      # TODO: 変数content_typeがコメントがないとわかり辛いので修正
+      @content_type = content_type # params[:value] セレクトボックス選択値
       @randoku_memo_type = randoku_memo_type
       @seidoku_memo_type = seidoku_memo_type
+
       selected_memos =
-        case @content_type[0]
-        when /^randoku\[\d+\]$/
-          index = @content_type[0]&.scan(/\d+/)&.first&.to_i
-          all_randoku_state_books.map {|book|
-            book.randoku_memos.where(content_state: index)
-          }.flatten
-        when /^seidoku\[\d+\]$/
-          index = @content_type[0]&.scan(/\d+/)&.first&.to_i
-          all_randoku_state_books.map {|book|
-            book.seidoku_memos.where(content_state: index)
-          }.flatten
-        end || []
+      case @content_type[0]
+      when /^randoku\[\d+\]$/
+        index = @content_type[0].scan(/\d+/).first.to_i
+        all_randoku_state_books.map {|book|
+          book.randoku_memos.where(content_state: index)
+        }.flatten
+      when /^seidoku\[\d+\]$/
+        index = @content_type[0].scan(/\d+/).first.to_i
+        all_randoku_state_books.map {|book|
+          book.seidoku_memos.where(content_state: index)
+        }.flatten
+      end
+
       @selected_memos_count = selected_memos.count || 0
+
       @selected_memos =
-        selected_memos&.map do |memo|
-          book = Book.find_by(id: memo.book_id)
-          reading_progress = book.reading_state || 0
-          # TODO: hashをクラスにする
+      if seleted_memos.present?
+        selected_memos.map { |memo|
+          mbook = Book.find_by(id: memo.book_id)
+          reading_progress = mbook.reading_state
+          #TODO: hashをクラスにする
           { content: memo.content,
             created_at: I18n.l(memo.created_at, format: :short),
-            # TODO: reading_state 0,2 を文字列から数字に詰め替えるメソッドで表現する
             content_state:
+              # TODO: 数字ではなくドメイン名を使用する
               if ['0', '2'].include?(reading_progress)
                 RANDOKU_MEMO_TYPE[memo.content_state]
               else
@@ -39,7 +44,9 @@ module ViewModel
             book_author: book.author_1,
             book_reading_progress: reading_progress
           }
-        end || []
+        }
       end
+
+    end #initialize
   end
 end
