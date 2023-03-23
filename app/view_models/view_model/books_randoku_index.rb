efdb3a4782043bd4cba7f0352e8ba114d4ec261e
sandonemaki <kamuramaki@gmail.com>
2023-03-23 23:02:11 +0900

    インデントを修正

--- app/view_models/view_model/books_randoku_index.rb
+++ app/view_models/view_model/books_randoku_index.rb
@@ -27,30 +27,29 @@ module ViewModel
         .order('COUNT(randoku_imgs.id) DESC').limit(3).pluck(:id)
 
 
-        # 「前回の続き」用
-        # TODO: ハッシュをクラスにする
-        @randoku_history =
-          { titile: randoku_history.title,
-            randoku_imgs_count: randoku_history.randoku_imgs.count,
-            randoku_memos_count: randoku_history.randoku_memos.count,
-            seidoku_memos_count: randoku_history.seidoku_memos.count,
-            reading_state: case randoku_history.reading_state
-            when State::READING_STATE.key("乱読")
-              "乱読"
-            when State::READING_STATE.key("精読")
-              "精読"
-            else
-              "通読"
-            end,
-            path: RandokuHistory.last.path,
-            randoku_history_ranking: randoku_img_ranking.include?(randoku_history.id) ?
-            randoku_img_ranking.index(randoku_history.id)+1 : ""
-          }
-      end
+      # 「前回の続き」用
+      # TODO: ハッシュをクラスにする
+      @randoku_history =
+        { titile: randoku_history.title,
+          randoku_imgs_count: randoku_history.randoku_imgs.count,
+          randoku_memos_count: randoku_history.randoku_memos.count,
+          seidoku_memos_count: randoku_history.seidoku_memos.count,
+          reading_state: case randoku_history.reading_state
+          when State::READING_STATE.key("乱読")
+            "乱読"
+          when State::READING_STATE.key("精読")
+            "精読"
+          else
+            "通読"
+          end,
+          path: RandokuHistory.last.path,
+          randoku_history_ranking: randoku_img_ranking.include?(randoku_history.id) ?
+          randoku_img_ranking.index(randoku_history.id)+1 : ""
+        }
 
       # 全ての乱読画像合計数
       @all_randoku_imgs_count =
-      Book.includes(:randoku_imgs).where.not(reading_state: "1").sum("randoku_imgs.id")
+        Book.includes(:randoku_imgs).where.not(reading_state: "1").sum("randoku_imgs.id")
 
       # 現在乱読ステータス中の本の中から乱読画像が多い順に並べる
       imgs_desc_of_randoku_state_books =
@@ -68,7 +67,7 @@ module ViewModel
       created_imgs_desc_of_randoku_state_books =
         all_randoku_state_books.find(
           RandokuImg.order('created_at desc').pluck(:book_id)
-      )
+        )
       @created_imgs_desc_of_randoku_state_books =
         created_imgs_desc_of_randoku_state_books.map do |book|
           { titile: book.title, randoku_imgs_count: book.randoku_imgs.count, randoku_memos_count: book.randoku_memos.count }
@@ -78,7 +77,7 @@ module ViewModel
       created_books_desc_of_randoku_state_books =
         all_randoku_state_books.find(
           Book.all.order('created_at desc').pluck(:id)
-      )
+        )
       @created_books_desc_of_randoku_state_books =
         created_books_desc_of_randoku_state_books.map do |book|
           { titile: book.title, randoku_imgs_count: book.randoku_imgs.count, randoku_memos_count: book.randoku_memos.count }
