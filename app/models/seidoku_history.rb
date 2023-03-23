a2b04bfc2d5f1942c2366ea4e46f6894520eb3a3
sandonemaki <kamuramaki@gmail.com>
2023-03-23 23:49:57 +0900

    精読学習履歴のインスタンス変数を修正

--- app/models/seidoku_history.rb
+++ app/models/seidoku_history.rb
@@ -1,9 +1,9 @@
 class SeidokuHistory < ApplicationRecord
 
   def self.set(new_path, book_id)
-    randoku_history = SeidokuHistory.order(:created_at).first_or_initialize
-    randoku_history.path = new_path
-    randoku_history.book_id = book_id
-    randoku_history.save
+    seidoku_history = SeidokuHistory.order(:created_at).first_or_initialize
+    seidoku_history.path = new_path
+    seidoku_history.book_id = book_id
+    seidoku_history.save
   end
 end
