b2a1729b703f23453ed2ddcce0194d96dec1d0a7
sandonemaki <kamuramaki@gmail.com>
2023-03-25 18:01:23 +0900

    検索に失敗した場合にもセレクトボックスで選択した検索menuが選択された状態にした

--- app/view_models/view_model/home_memo_search.rb
+++ app/view_models/view_model/home_memo_search.rb
@@ -1,11 +1,14 @@
 module ViewModel
   class HomeMemoSearch
-    attr_reader :randoku_memo_type, :seidoku_memo_type, :selected_memos
+    attr_reader :randoku_memo_type, :seidoku_memo_type, :selected_memos,
+      :randoku_memo_selected_menu, :seidoku_memo_selected_menu
 
     def initialize(randoku_memo_type:, seidoku_memo_type:)
       @randoku_memo_type = randoku_memo_type
       @seidoku_memo_type = seidoku_memo_type
       @selected_memos = []
+      @randoku_memo_selected_menu = ""
+      @seidoku_memo_selected_menu = ""
     end
   end
 end
