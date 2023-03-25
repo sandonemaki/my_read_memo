678669d404944eb17ce54809be553434e57bf100
sandonemaki <kamuramaki@gmail.com>
2023-03-26 01:53:39 +0900

    search修正

--- app/view_models/view_model/home_memo_search_result.rb
+++ app/view_models/view_model/home_memo_search_result.rb
@@ -1,59 +1,58 @@
 module ViewModel
   # todo:要挙動確認。view_modelではなくクラス化必要?
-  class HomeMemoSearch_result
-    attr_reader :selected_memos_count, :randoku_memo_type, :seidoku_memo_type, :selected_memos
+  class HomeMemoSearchResult
+    attr_reader :selected_search_value, :randoku_memo_type, :seidoku_memo_type,
+      :selected_memos_count, :selected_memos
 
-    def initialize(all_randoku_state_books:, all_seidoku_state_books:, selected_search_value:,
-                   randoku_memo_type:, seidoku_memo_type:)
-      # params[value]
+    def initialize(all_randoku_state_books:, all_seidoku_state_books:,
+                   randoku_memo_type:, seidoku_memo_type:, selected_search_value:)
+
       @selected_search_value = selected_search_value
       @randoku_memo_type = randoku_memo_type
       @seidoku_memo_type = seidoku_memo_type
 
       selected_memos =
-        case @content_type[0]
+        case selected_search_value
         when /^randoku\[\d+\]$/
-          index = @content_type[0].scan(/\d+/).first.to_i
+          index = selected_search_value.scan(/\d+/).first.to_i
           all_randoku_state_books.map {|book|
             book.randoku_memos.where(content_state: index)
           }.flatten
         when /^seidoku\[\d+\]$/
-          index = @content_type[0].scan(/\d+/).first.to_i
+          index = selected_search_value.scan(/\d+/).first.to_i
           all_randoku_state_books.map {|book|
             book.seidoku_memos.where(content_state: index)
           }.flatten
         end
 
-      @selected_memos_count = selected_memos.count || 0
+      @selected_memos_count = selected_memos&.count || 0
       @selected_memos =
-        if seleted_memos.present?
+        if selected_memos.present?
           selected_memos.map { |memo|
-            book = Book.find_by(id: memo.book_id)
-            ViewModel::SelectedMemo.new(memo: memo, book: book)
+            ViewModel::SelectedMemo.new(memo: memo, book: Book.find_by(id: memo.book_id))
           }
         end
     end #initialize
-
   end
 
   class SelectedMemo
-    attr_reader :content, :created_at, :book_reading_progress, :content_type,
-      :book_title, :book_author
+    attr_reader :memo_content, :memo_created_at, :book_reading_progress,
+      :memo_content_type, :book_title, :book_author
 
     def initialize(memo:, book:)
-      @content = memo.content
-      @created_at = I18n.l(memo.created_at, format: :short)
+      @memo_content = memo.content
+      @memo_created_at = I18n.l(memo.created_at, format: :short)
       @book_reading_progress = book.reading_state
-      @content_type =
+      @memo_content_type =
         # TODO: intではなくモデリング名を使用する
         if ['0', '2'].include?(@reading_progress)
-          RANDOKU_MEMO_TYPE[memo.content_state]
+          State::RANDOKU_MEMO_TYPE[memo.content_state]
         else
-          SEIDOKU_MEMO_TYPE[memo.content_state]
+          State::SEIDOKU_MEMO_TYPE[memo.content_state]
         end
       @book_title = book.title
       @book_author = book.author_1
     end
   end
 
-end
+end #ViewModel
