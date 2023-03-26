428f0851730dd520fb507bcd7164f6788a38835e
sandonemaki <kamuramaki@gmail.com>
2023-03-27 03:01:50 +0900

    検索前/検索結果が0件/検索結果が1件以上で表示を変更

--- app/view_models/view_model/home_memo_search.rb
+++ app/view_models/view_model/home_memo_search.rb
@@ -7,7 +7,7 @@ module ViewModel
       @randoku_memo_type = randoku_memo_type
       @seidoku_memo_type = seidoku_memo_type
       @selected_search_value = ""
-      @selected_memos = []
+      @selected_memos = nil
       @randoku_memo_selected_menu = ""
       @seidoku_memo_selected_menu = ""
     end
