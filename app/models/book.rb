fe1329725d995fe40ca61a2d2e80322319d659f0
sandonemaki <kamuramaki@gmail.com>
2023-04-06 21:48:23 +0900

    total_pageのvalidationを追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,6 +6,8 @@ class Book < ApplicationRecord
   validates :title, length: {maximum: 30, message: "30文字以内で入力してください"}
   validates :author_1, presence: {message: "著者を入力してください"}
   validates :author_1, length: {maximum: 30, message: "30文字以内で入力してください"}
+  validates :total_page, presence: {message: "読む予定のページ数を入力してください"}, inclusion: { in: 1..999,
+  message: "登録できるページ数は999ページまでです" }
 
   def try_update_reading_state(book:, judgement_type:)
     new_reading_state = case judgement_type
