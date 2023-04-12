3392c1836b676d722cf2ae5075a45be6ff49f61e
sandonemaki <kamuramaki@gmail.com>
2023-04-13 02:41:44 +0900

    精読ヒストリーのsetメソッドのcontext,itを作成

--- spec/models/seidoku_history_spec.rb
+++ spec/models/seidoku_history_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe SeidokuHistory, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe ".set" do
+    context "精読ヒストリーのレコードが存在しない場合" do
+      it "精読ヒストリーのレコードの数が1件増える"
+    end
+    
+    context "精読ヒストリーのレコードが存在する場合" do
+      it "精読ヒストリーのレコードの数が変わらないこと"
+    end
+
+  end
 end
