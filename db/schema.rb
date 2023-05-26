1eb2d6ba9da9ae79ef2e27c14765ada6d1ed9000
sandonemaki <kamuramaki@gmail.com>
2023-05-26 22:56:47 +0900

    booksテーブルに初めて精読状態になった時に精読メモの鍵を外すカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_05_100330) do
+ActiveRecord::Schema.define(version: 2023_05_26_131502) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2022_11_05_100330) do
     t.integer "reading_state", default: 0, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "seidoku_memo_key", default: true
   end
 
   create_table "randoku_histories", force: :cascade do |t|
