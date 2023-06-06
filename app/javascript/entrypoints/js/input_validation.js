9c293e9ac684273761986e625b9bda09bb849334
sandonemaki <kamuramaki@gmail.com>
2023-06-06 18:55:14 +0900

    jsでタイトルのバリデーションを追加

--- app/javascript/entrypoints/js/input_validation.js
+++ app/javascript/entrypoints/js/input_validation.js
@@ -1,3 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
+  // book/newのタイトル
+  const inputTitle = document.querySelector('#input_title');
+  const titleValidError = document.querySelector('#title_valid_error');
+
+  const isInputTitleLengthValid = (inputTitle, minLength = 0, maxLength = 20) => {
+    return inputTitle.value.length >= minLength && inputTitle.value.length <= maxLength;
+  }
+  inputTitle.addEventListener('input', () => {
+    if (isInputTitleLengthValid(inputTitle) === false) {
+      titleValidError.textContent = 'タイトルは20文字以内で入力してください';
+    } else {
+      titleValidError.textContent = '';
+    }
+    checkValidation();
+  });
 
 });
