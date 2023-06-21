35b2210fae6f5161af5a2c07fc75e0e4dd76d445
sandonemaki <kamuramaki@gmail.com>
2023-06-21 17:36:56 +0900

    zoom in/out関数を追加

--- app/javascript/entrypoints/js/panzoom.js
+++ app/javascript/entrypoints/js/panzoom.js
@@ -9,3 +9,8 @@ export const initializePanzoom = (panzoomEl) => {
     //initialZoom: 1,
   });
 };
+// zoom_in/out関数
+export const zoomInOut = (panzoomEl, instance, isIn) => {
+  const scale = 1 + 0.25 * (isIn ? 1 : -1);
+  instance.zoomTo(panzoomEl.clientWidth / 2, panzoomEl.clientHeight / 2, scale);
+};
