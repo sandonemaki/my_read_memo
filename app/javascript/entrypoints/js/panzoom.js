d35dc4f9de2183c75b7407bc2f1fa809e6626cad
sandonemaki <kamuramaki@gmail.com>
2023-06-21 17:37:16 +0900

    zoom reset関数を追加

--- app/javascript/entrypoints/js/panzoom.js
+++ app/javascript/entrypoints/js/panzoom.js
@@ -14,3 +14,10 @@ export const zoomInOut = (panzoomEl, instance, isIn) => {
   const scale = 1 + 0.25 * (isIn ? 1 : -1);
   instance.zoomTo(panzoomEl.clientWidth / 2, panzoomEl.clientHeight / 2, scale);
 };
+
+// zoom_reset関数
+export const zoomReset = (instance) => {
+  // Reset the panzoom element to its initial position and scale
+  instance.moveTo(0, 0);
+  instance.zoomAbs(0, 0, 1); // 1 is the initial scale
+};
