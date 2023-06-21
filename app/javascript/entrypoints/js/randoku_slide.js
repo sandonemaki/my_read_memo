e86ed83b45ed3495defeed79a462d8ee2c60fa4d
sandonemaki <kamuramaki@gmail.com>
2023-06-21 17:19:00 +0900

    panzoomを使うため拡大縮小する画像のエレメントを取得

--- app/javascript/entrypoints/js/randoku_slide.js
+++ app/javascript/entrypoints/js/randoku_slide.js
@@ -4,12 +4,10 @@ import { js_flash, js_flash_alert } from './flash.js';
 
 document.addEventListener('DOMContentLoaded', function () {
   const swiper = new Swiper('.swiper', {
-    slidesPerView: 'auto', //スライドの余白を自動調整
-    centeredSlides: true, //中央揃え
-    resizeObserver: true, //コンテナのリサイズ。スマホ
-    zoom: {
-      enabled: true,
-    },
+    slidesPerView: 'auto',
+    centeredSlides: true,
+    resizeObserver: true,
+    simulateTouch: false, // disable swiper's drag and drop
     height: 'auto',
     pagination: {
       el: '.swiper-pagination',
@@ -19,6 +17,19 @@ document.addEventListener('DOMContentLoaded', function () {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
+  });
+
+  swiper.on('slideChange', function () {
+    // 現在のスライドを取得
+    const currentSlide = this.slides[this.activeIndex];
+    const panzoomEl = currentSlide.querySelector('.panzoom');
+    const imgEl = panzoomEl.querySelector('.panzoom-img');
+    const printZoomEl = currentSlide.querySelector('.print-zoom');
+
+    // zoomの操作ボタンを取得
+    const zoomInEl = document.querySelector('.zoomIn');
+    const zoomOutEl = document.querySelector('.zoomOut');
+    const zoomResetEl = document.querySelector('.zoomReset');
   });
 
   // モーダルを開く。クリックされた画像からスライドを始める
