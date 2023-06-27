8e8e802dd6bb118e1238a378262c6a4f6979aab7
sandonemaki <kamuramaki@gmail.com>
2023-06-27 20:38:58 +0900

    精読メモの中で乱読メモの多い上位３位に王冠マークをつけた

--- app/view_models/view_model/books_seidoku_index_rank_most_randoku_imgs.rb
+++ app/view_models/view_model/books_seidoku_index_rank_most_randoku_imgs.rb
@@ -11,13 +11,21 @@ module ViewModel
         .select('books.*, COUNT(randoku_imgs.id) as randoku_imgs_count')
         .order('randoku_imgs_count DESC')
 
+      # 現在精読中の本の中から乱読画像が多い順のbook_id。1-3位まで
+      randoku_img_ranking = all_seidoku_state_books.joins(:randoku_imgs)
+        .group('books.id')
+        .select('books.id, COUNT(randoku_imgs.id) as count')
+        .order('COUNT(randoku_imgs.id) DESC').limit(3).pluck(:id)
+      
       #TODO: ハッシュをクラスにする
       @books_index_rank =
         most_randoku_memos_desc_of_seidoku_state_books.map do |book|
           {
             title: book.title,
             randoku_imgs_count: book.randoku_imgs.count,
-            seidoku_memos_count: book.seidoku_memos.count
+            seidoku_memos_count: book.seidoku_memos.count,
+            randoku_ranking: randoku_img_ranking.include?(book.id) ?
+            randoku_img_ranking.index(book.id)+1 : ""
           }
         end
 
