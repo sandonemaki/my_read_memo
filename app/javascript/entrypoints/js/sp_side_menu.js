a4cb5180ee823625e57c22017835b701c61ff99d
sandonemaki <kamuramaki@gmail.com>
2023-07-05 20:11:28 +0900

    side-menuをopenするボタンのクリック時、移動するコンテンツが複数わることを見込んで修正

--- app/javascript/entrypoints/js/sp_side_menu.js
+++ app/javascript/entrypoints/js/sp_side_menu.js
@@ -3,17 +3,21 @@ document.addEventListener('DOMContentLoaded', (event) => {
   openSlideMenuList.forEach((menu) => {
     menu.addEventListener('click', function () {
       const header = document.querySelector('#header-content-js');
-      const main = document.querySelector('#main-background');
-      const globalSlider = document.querySelector('#global-sidebar'); // querySelectorAllをquerySelectorに変更
-      if (header.style.transform === 'translate(-280px)' && main.style.transform === 'translate(-280px)') {
+      const mains = document.querySelectorAll('.main-content-js');
+      const globalSlider = document.querySelector('#global-sidebar');
+      if (header.style.transform === 'translate(-280px)') {
         header.style.transform = 'translate(0px)';
-        main.style.transform = 'translate(0px)';
+        mains.forEach((main) => {
+          main.style.transform = 'translate(0px)';
+        });
         //globalSlider.style.opacity = '0';
         //globalSlider.style.visibility = 'hidden';
         //globalSlider.style.zIndex = -100;
       } else {
         header.style.transform = 'translate(-280px)';
-        main.style.transform = 'translate(-280px)';
+        mains.forEach((main) => {
+          main.style.transform = 'translate(-280px)';
+        });
         globalSlider.style.opacity = '1';
         globalSlider.style.visibility = 'visible';
       }
