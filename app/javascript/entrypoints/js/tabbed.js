ea143b600e936eb7d7d707996c7dfd221b76e720
sandonemaki <kamuramaki@gmail.com>
2023-07-06 15:03:38 +0900

    showページのタブデザインを変更

--- app/javascript/entrypoints/js/tabbed.js
+++ app/javascript/entrypoints/js/tabbed.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
   const tabMenuItems = document.querySelectorAll('.tab__menu-item');
+  const underline = document.querySelector('#underline'); // 追加
   const tabPanelItems = document.querySelectorAll('.tab__panel-box');
   const tabPanelBtns = document.querySelectorAll('.index_tab__panel-btn-js');
 
@@ -9,21 +10,24 @@ document.addEventListener('DOMContentLoaded', function () {
     tabPanelBtns.forEach((item) => item.classList.remove('is-show'));
   }
 
-  function tabSwitch(e) {
+  function tabSwitch(e, index) {
     clearActiveStates();
+    underline.style.left = `${index * 33.33}%`;
     const tabTargetData = e.currentTarget.dataset.tab;
 
     e.currentTarget.classList.add('is-active');
-    const panel = document.querySelector(`.tab__panel-box[data-panel="${tabTargetData}"]`);
-    const btns = document.querySelectorAll(`.index_tab__panel-btn-js[data-btn="${tabTargetData}"]`);
+    const panelToShow = document.querySelector(`.tab__panel-box[data-panel="${tabTargetData}"]`);
+    const btnsToShow = document.querySelectorAll(`.index_tab__panel-btn-js[data-btn="${tabTargetData}"]`);
 
-    if (panel) panel.classList.add('is-show');
-    btns.forEach((btn) => {
-      if (btn) btn.classList.add('is-show');
+    if (panelToShow) panelToShow.classList.add('is-show');
+    btnsToShow.forEach((btnToShow) => {
+      if (tnToShow) tnToShow.classList.add('is-show');
     });
   }
 
-  tabMenuItems.forEach((tabMenuItem) => {
-    tabMenuItem.addEventListener('click', tabSwitch);
+  tabMenuItems.forEach((item, index) => {
+    item.addEventListener('click', function (e) {
+      tabSwitch(e, index);
+    });
   });
 });
