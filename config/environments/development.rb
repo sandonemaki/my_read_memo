0be2acd158450f33d545aa61b926c0a5775be246
sandonemaki <kamuramaki@gmail.com>
2023-07-19 23:33:58 +0900

    railsがドメインの接続を許すように設定

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,4 +71,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  # 自分で追加 230718
+  config.hosts << "localhost.yondeco.site"
 end
