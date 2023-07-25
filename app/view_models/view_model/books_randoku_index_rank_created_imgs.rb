f49ed4cba93556b1cf72dd8fbadcef6d07e6a1a4
sandonemaki <kamuramaki@gmail.com>
2023-07-26 00:36:59 +0900

    乱読本の上位３位まで王冠を追加

--- app/view_models/view_model/books_randoku_index_rank_created_imgs.rb
+++ app/view_models/view_model/books_randoku_index_rank_created_imgs.rb
@@ -12,6 +12,12 @@ module ViewModel
         .group('books.id')
         .order('latest_randoku_img_time DESC')
 
+      # 現在乱読中の本の中から乱読画像が多い順のbook_id。1-3位まで
+      randoku_img_ranking = all_randoku_state_books.joins(:randoku_imgs)
+        .group('books.id')
+        .select('books.id, COUNT(randoku_imgs.id) as count')
+        .order('COUNT(randoku_imgs.id) DESC').limit(3).pluck(:id)
+
       #TODO: ハッシュをクラスにする
       @books_index_rank =
         created_imgs_desc_of_randoku_state_books.map do |book|
@@ -23,6 +29,16 @@ module ViewModel
             title: book.title,
             randoku_imgs_count: book.randoku_imgs.count,
             randoku_memos_count: book.randoku_memos.count,
+            reading_state: case book.reading_state
+            when State::READING_STATE.key("乱読")
+              "乱読"
+            when State::READING_STATE.key("精読")
+              "精読"
+            else
+              "通読"
+            end,
+            randoku_ranking: randoku_img_ranking.include?(book.id) ?
+            randoku_img_ranking.index(book.id)+1 : "",
             # 精読まで未読をあと何枚
             remaining: book.countdown_remaining_seidoku
           }
