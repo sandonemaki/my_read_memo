c943bcf1241e21789d9586b5ab015655067a5f3a
sandonemaki <kamuramaki@gmail.com>
2023-08-06 22:04:02 +0900

    docker使用時もlogがサーバー起動のターミナルページに出力されるよう設定

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,6 +71,9 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  # 追加：ログの出力先をターミナルに設定
+  config.logger = ActiveSupport::Logger.new(STDOUT)
+
   # 自分で追加 230718
   config.hosts << 'localhost.yondeco.site'
 end
