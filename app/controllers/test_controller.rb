a3675c4d826753b661cb321ace3817fbaf627a39
sandonemaki <kamuramaki@gmail.com>
2023-08-06 22:38:49 +0900

    不要なcontrollerを削除

--- app/controllers/test_controller.rb
+++
@@ -1,8 +0,0 @@
-class TestController < ApplicationController
-  def index
-    book = Book.find_by(id: 3)
-    new_path = "books/#{book.id}"
-    book_view_model = ViewModel::TestBookRandokuImgs.new(book: book)
-    render('index', locals: { book: book_view_model })
-  end
-end
