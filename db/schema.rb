3b520d24062095cdb688a9f1668569d521a7c431
sandonemaki <kamuramaki@gmail.com>
2023-08-22 16:10:57 +0900

    usersテーブルにauth0の一意の値subを保存するauth0_idカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_17_093636) do
+ActiveRecord::Schema.define(version: 2023_08_22_070240) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,6 +76,7 @@ ActiveRecord::Schema.define(version: 2023_08_17_093636) do
     t.string "nickname"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "auth0_id"
   end
 
   add_foreign_key "books", "users"
