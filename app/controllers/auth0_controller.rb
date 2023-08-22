88c9462ef51dbc6938be4bf31271ff41257eb03d
sandonemaki <kamuramaki@gmail.com>
2023-08-22 16:58:54 +0900

    nickname は最大で13文字まで保存

--- app/controllers/auth0_controller.rb
+++ app/controllers/auth0_controller.rb
@@ -8,7 +8,9 @@ class Auth0Controller < ApplicationController
     # for complete information on 'omniauth.auth' contents.
     auth_info = request.env['omniauth.auth']
     session[:userinfo] = auth_info['extra']['raw_info']
-    User.find_or_create_by(auth0_id: session[:userinfo][:sub]) { |u| u.nickname = session[:userinfo][:nickname] }
+    User.find_or_create_by(auth0_id: session[:userinfo][:sub]) do |u|
+      u.nickname = session[:userinfo][:nickname].slice(0, 13)
+    end
 
     # Redirect to the URL you want after successful auth
     redirect_to '/books/randoku_index'
