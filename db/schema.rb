1113ce146ce880346aa25fcbc5745e41ec3dc909
sandonemaki <kamuramaki@gmail.com>
2023-08-22 16:36:42 +0900

    usersテーブル側でauth0がnull falseかつuniqueに設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_22_070240) do
+ActiveRecord::Schema.define(version: 2023_08_22_073405) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,7 +76,8 @@ ActiveRecord::Schema.define(version: 2023_08_22_070240) do
     t.string "nickname"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "auth0_id"
+    t.string "auth0_id", null: false
+    t.index ["auth0_id"], name: "index_users_on_auth0_id", unique: true
   end
 
   add_foreign_key "books", "users"
