74af0b78528be41172f2ee6b9d3ebb1133b1f498
sandonemaki <kamuramaki@gmail.com>
2023-08-30 19:00:57 +0900

    テスト対象を修正

--- spec/models/randoku_memo_spec.rb
+++ spec/models/randoku_memo_spec.rb
@@ -3,11 +3,9 @@ require 'rails_helper'
 RSpec.describe RandokuMemo, type: :model do
   describe 'バリデーション' do
     # テスト用のUserオブジェクトを作成
-    before { @user = User.create!(auth0_id: 'auth0|1234', nickname: 'TestUser') }
+    let(:user) { User.create!(auth0_id: 'auth0|1234', nickname: 'TestUser') }
 
-    subject(:valid?) { randoku_memo.valid? }
-
-    let(:book) { Book.create(title: 'ダイニングタイトル', author_1: '東野智子', total_page: 100, user_id: @user.id) }
+    let(:book) { Book.create(title: 'ダイニングタイトル', author_1: '東野智子', total_page: 100, user_id: user.id) }
 
     let(:randoku_memo) { RandokuMemo.new(content: content, book: book) }
     let(:content) do
@@ -16,21 +14,21 @@ RSpec.describe RandokuMemo, type: :model do
 
     context 'すべての属性が有効なとき' do
       it 'バリデーションが通ること' do
-        expect(book.valid?).to eq true
+        expect(randoku_memo.valid?).to eq true
       end
     end
 
     context 'コンテンツが存在しないとき' do
       let(:content) { nil }
       it 'バリデーションエラーが発生すること' do
-        expect(book.valid?).to eq false
+        expect(randoku_memo.valid?).to eq false
       end
     end
 
     context 'コンテキストが空のとき' do
       let(:content) { '' }
       it 'バリデーションエラーが発生すること' do
-        expect(book.valid?).to eq false
+        expect(randoku_memo.valid?).to eq false
       end
     end
   end
