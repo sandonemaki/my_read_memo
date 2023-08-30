d04e57f059d3fe7d6c33e4656d24975535b5377e
sandonemaki <kamuramaki@gmail.com>
2023-08-30 19:01:34 +0900

    テスト対象を修正

--- spec/models/seidoku_memo_spec.rb
+++ spec/models/seidoku_memo_spec.rb
@@ -5,9 +5,9 @@ RSpec.describe SeidokuMemo, type: :model do
     subject(:valid?) { seidoku_memo.valid? }
 
     # テスト用のUserオブジェクトを作成
-    before { @user = User.create!(auth0_id: 'auth0|1234', nickname: 'TestUser') }
+    let(:user) { User.create!(auth0_id: 'auth0|1234', nickname: 'TestUser') }
 
-    let(:book) { Book.create(title: 'ダイニングタイトル', author_1: '東野智子', total_page: 100, user_id: @user.id) }
+    let(:book) { Book.create(title: 'ダイニングタイトル', author_1: '東野智子', total_page: 100, user_id: user.id) }
 
     let(:seidoku_memo) { SeidokuMemo.new(content: content, book: book) }
     let(:content) do
@@ -16,21 +16,21 @@ RSpec.describe SeidokuMemo, type: :model do
 
     context 'すべての属性が有効なとき' do
       it 'バリデーションが通ること' do
-        expect(book.valid?).to eq true
+        expect(seidoku_memo.valid?).to eq true
       end
     end
 
     context 'コンテンツが存在しないとき' do
       let(:content) { nil }
       it 'バリデーションエラーが発生すること' do
-        expect(book.valid?).to eq false
+        expect(seidoku_memo.valid?).to eq false
       end
     end
 
     context 'コンテキストが空のとき' do
       let(:content) { '' }
       it 'バリデーションエラーが発生すること' do
-        expect(book.valid?).to eq false
+        expect(seidoku_memo.valid?).to eq false
       end
     end
   end
